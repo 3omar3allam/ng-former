fd2e3c26c3d74fdf4bd5012cb0dc2f133eff5ad6
Omar <omarallam96@gmail.com>
2022-01-11 15:55:00 +0200

    unsubscribe from observables (bug fix)

--- src/lib/form-builder/canvas/canvas.component.ts
+++ src/lib/form-builder/canvas/canvas.component.ts
@@ -1,5 +1,7 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { FieldBase } from '../../shared/field';
 import { FormBuilderService } from '../form-builder.service';
 
@@ -8,28 +10,38 @@ import { FormBuilderService } from '../form-builder.service';
   templateUrl: './canvas.component.html',
   styleUrls: ['../form-builder.component.scss', './canvas.component.scss'],
 })
-export class CanvasComponent implements OnInit {
+export class CanvasComponent implements OnInit, OnDestroy {
   @Input() fields!: FieldBase<any>[];
 
+  private unsubscribeAll$ = new Subject<void>();
   constructor(
     private fbs: FormBuilderService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.fbs.fieldCreated$.subscribe(data => {
-      const field = this.fbs.castPerType(data.field);
-      if (data.index != null && this.fields[data.index]) {
-        this.fields[data.index] = field;
-      } else {
-        this.fields.push(field)
-      }
-    });
+    this.fbs.fieldCreated$
+      .pipe(takeUntil(this.unsubscribeAll$))
+      .subscribe(data => {
+        const field = this.fbs.castPerType(data.field);
+        if (data.index != null && this.fields[data.index]) {
+          this.fields[data.index] = field;
+        } else {
+          this.fields.push(field)
+        }
+      });
 
-    this.fbs.fieldDeleted$.subscribe(index => {
-      if (this.fields[index]) {
-        this.fields.splice(index, 1);
-      }
-    });
+    this.fbs.fieldDeleted$
+      .pipe(takeUntil(this.unsubscribeAll$))
+      .subscribe(index => {
+        if (this.fields[index]) {
+          this.fields.splice(index, 1);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll$.next();
+    this.unsubscribeAll$.complete();
   }
 
   drop(event: CdkDragDrop<FieldBase<any>[]>) {
